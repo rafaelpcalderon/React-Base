5985fa3960bdb77b7de69e08abc4d9e0c0eae166
Rafael Calderon <142346012+rafaelpcalderon@users.noreply.github.com>
2025-06-01 15:56:49 -0300

    Update Paragraph.jsx

--- src/components/Paragraph/Paragraph.jsx
+++ src/components/Paragraph/Paragraph.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function Paragraph(props) {
     return(
         <>
-            <p className="colorText" style={{color: props.color}}> 
+            <p className="colorText" style={{color: props.color, textAlign: 'center'}}> 
                 {props.text.toUpperCase()} 
             </p>
         </>
